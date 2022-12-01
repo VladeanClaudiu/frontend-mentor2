634a5066a0cf5e95838cc99d7fa83aafe496408d
VladeanClaudiu <claudiuver3.2@gmail.com>
2022-12-01 16:00:31 +0000

    scss installed

--- tip-calculator-react-app/src/App.jsx
+++ tip-calculator-react-app/src/App.jsx
@@ -1,32 +1,46 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className ="container">
+      <h1>SPLI<br />TTER</h1>
+      <div className="main-app">
+        <div className="main-tip">
+          <p className="headings">Bill</p>
+          <input name="bill" className="bill-input" type="number" placeholder="0" />
+        </div>
+
+        <div className="main-select-tip">
+          <p className="headings">Select Tip %</p>
+          <div className="tip-selections">
+            <button>5%</button>
+            <button>10%</button>
+            <button>15%</button>
+            <button>25%</button>
+            <button>50%</button>
+            <input
+              name="custom-tip"
+              className="bill-input"
+              type="number"
+              placeholder="Custom"
+            />
+          </div>
+        </div>
+        <div className="main-number-people">
+          <p className="headings">Number of People</p>
+          <input
+            name="people"
+            className="people-input"
+            type="number"
+            placeholder="0"
+          />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </div>
   )
 }
