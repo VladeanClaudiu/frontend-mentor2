a91c33c0b1b192986b52ddaad4a19bb4e70dfea5
VladeanClaudiu <claudiuver3.2@gmail.com>
2022-12-02 17:07:59 +0000

    Main logic is complete, some cleanup needed to get rid of small bugs

--- tip-calculator-react-app/src/App.jsx
+++ tip-calculator-react-app/src/App.jsx
@@ -1,8 +1,41 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [bill, setBill] = useState("")
+  const [numberOfPeople, setNumberOfPeople] = useState("")
+  const [customPercent, setCustomPercent] = useState("")
+
+  const [tipAmount, setTipAmount] = useState({tip: 0.00, total: 0.00})
+
+  function handleBillChange(e) {
+    setBill(e.target.value)
+  }
+
+  function handlePeopleChange(e) {
+    setNumberOfPeople(e.target.value)
+  }
+
+  function handleCustomPercent(e) {
+    setCustomPercent(e.target.value)
+  }
+
+  useEffect(() => {
+   const totalBill = (bill*(100+Number(customPercent)))/100
+   const totalPlusTip = (totalBill/numberOfPeople).toFixed(2)
+   const tipPerPerson = ((totalBill - bill) / numberOfPeople).toFixed(2)
+
+   console.log(totalBill, totalPlusTip, tipPerPerson)
+
+   setTipAmount(prevValue => ({tip: tipPerPerson, total: totalPlusTip}))
+  },[bill, numberOfPeople, customPercent])
+
+  function resetCalculator() {
+    setCustomPercent("")
+    setNumberOfPeople("")
+    setBill("")
+    setTipAmount({tip: 0.00, total: 0.00})
+  }
 
   return (
     <div className="App">
@@ -11,30 +44,30 @@ function App() {
       <div className="main-app">
         <div className="main-tip">
           <p className="headings">Bill</p>
-          <input name="bill" className="bill-input" type="number" placeholder="0" />
+          <input name="bill" className="bill-input" type="number" placeholder="0" value = {bill} onChange={handleBillChange}/>
         </div>
 
         <div className="main-select-tip">
           <p className="headings">Select Tip %</p>
           <ul className="tip-selections">
             <li>
-              <input type="radio" id='5p' name="percent"/>
+              <input type="radio" id='5p' name="percent" value={5} onChange ={handleCustomPercent}  />
               <label htmlFor="5p">5%</label>
             </li>
             <li>
-              <input type="radio" id='10p' name="percent"/>
+              <input type="radio" id='10p' name="percent" value={10} onChange ={handleCustomPercent}  />
               <label htmlFor="10p">10%</label>
             </li>
             <li>
-              <input type="radio" id='15p' name="percent"/>
+              <input type="radio" id='15p' name="percent" value={15} onChange ={handleCustomPercent}  />
               <label htmlFor="15p">15%</label>
             </li>
             <li>
-              <input type="radio" id='25p' name="percent"/>
+              <input type="radio" id='25p' name="percent" value={25} onChange ={handleCustomPercent}  />
               <label htmlFor="25p">25%</label>
             </li>
             <li>
-              <input type="radio" id='50p' name="percent"/>
+              <input type="radio" id='50p' name="percent" value={50} onChange ={handleCustomPercent}  />
               <label htmlFor="50p">50%</label>
             </li>
             <li>
@@ -42,8 +75,10 @@ function App() {
               name="custom-tip"
               className="bill-input"
               type="number"
-              placeholder="Custom"
-            />
+              placeholder="Custom"  
+              value={customPercent} 
+              onChange ={handleCustomPercent}        
+               />
             </li>
             
           </ul>
@@ -55,18 +90,20 @@ function App() {
             className="people-input"
             type="number"
             placeholder="0"
+            value={numberOfPeople}
+            onChange = {handlePeopleChange}
           />
         </div>
         <div className='final-cost-rest'>
           <div className='final-tip-amount'>
             <p>Tip Amount <br/><span>/ person</span></p>
-            <p className='total-price'>$0.00</p>
+            <p className='total-price'>${tipAmount.tip}</p>
           </div>
           <div className='final-total'>
-            <p>Tip Amount <br/><span>/ person</span></p>
-            <p className='total-price'>$0.00</p>
+            <p>Total <br/><span>/ person</span></p>
+            <p className='total-price'>${tipAmount.total}</p>
           </div>
-          <button disabled={false} >RESET</button>
+          <button disabled={false} onClick={resetCalculator}>RESET</button>
         </div>
       </div>
     </div>
