998490c1d1d105de8e41afa9867b3fe38e0521cc
VladeanClaudiu <claudiuver3.2@gmail.com>
2022-12-12 13:39:24 +0000

    customised head image for site added images to nav items and background image for head

--- react-portfolio-website/src/App.jsx
+++ react-portfolio-website/src/App.jsx
@@ -3,7 +3,27 @@ import './App.css'
 function App() {
 
   return (
-   <div className="HelloWOrld"><p>hi</p></div>
+   <div className="container">
+      <div className='page-head'>
+        <div className='logo-plus-social-media'>
+          <div className="logo">
+            <p>claudiu<span>vladean</span></p>
+          </div>
+          <div className='social-media'>
+            <ul>
+              <li className='git'></li>
+              <li className='linkedIn'></li>
+              <li className='frontendMentor'></li>
+              <li className='twitter'></li>
+            </ul>
+          </div>
+        </div>
+        <div className='head-image'>
+
+        </div>
+      </div>
+
+   </div>
   )
 }
 
